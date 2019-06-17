0d8ff4d2708c2ddd1891884030344633c092f68a
Jim Whiteman <jimtron9000@gmail.com>
2019-06-16 22:02:57 -0700

    Set 1, challenge 1

---
+++ set-1/challenge1.rb
@@ -0,0 +1,11 @@
+hex = "49276d206b696c6c696e6720796f757220627261696e206c" \
+      "696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+original =
+  hex.
+    scan(/../).
+    reduce("") do |acc, hex_byte|
+      acc << hex_byte.hex.chr
+    end
+
+puts [original].pack("m0")
