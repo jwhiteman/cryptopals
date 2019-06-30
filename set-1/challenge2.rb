fb4926c4443a13056c6ba4070782215a002af00e
Jim Whiteman <jimtron9000@gmail.com>
2019-06-29 17:05:02 -0700

    Add notes

--- set-1/challenge2.rb
+++ set-1/challenge2.rb
@@ -1,20 +1,28 @@
 a = "1c0111001f010100061a024b53535009181c"
 b = "686974207468652062756c6c277320657965"
 
-def fixed_xor(a, b)
+def fixed_xor_cheating(a, b)
   (a.to_i(16) ^ b.to_i(16)).to_s(16)
 end
+puts fixed_xor_cheating(a, b)
 
-puts fixed_xor(a, b)
-
-def fx(a, b)
-  h1 = a.scan(/../)
+def fixed_xor(a, b)
+  h1 = a.scan(/../) # chunk each hex string into "bytes"
   h2 = b.scan(/../)
 
-  # QUESTION: another way to achieve .hex?
-  m = h1.zip(h2).map { |l, r| l.hex ^ r.hex }.pack("U*")
+  # zip them together, so that each byte can be with its corresponding
+  # byte in the other string.
+  # convert each to a number (representing the bytes), and then XOR'ing
+  # them together, then finally use pack to convert the array-of-number-bytes
+  # into a series of unsigned chars (aka, the message)
+  m = h1.zip(h2).map { |l, r| l.hex ^ r.hex }.pack("C*")
 
+  # m
+  # => "the kid don't play"
+
+  # finally turn it back into hex
+  # unpack, here, returns an array with a single element, so we grab it
   m.unpack("H*").first
 end
 
-puts fx(a, b)
+puts fixed_xor(a, b)
--- set-1/challenge3.rb
+++ set-1/challenge3.rb
@@ -1,16 +1,26 @@
+# https://cryptopals.com/sets/1/challenges/3
 require "pry"
 require_relative "frequency"
 
+# this is our cipher text. it has been encrypted w/ a single char xor.
 c1 = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 
 results =
-  (0..255).map do |c|
-    h1 = c1.scan(/../)
+  (0..255).map do |c| # we'll iterate over 0-256 to look for the char
+    h1 = c1.scan(/../) # take c1 and chunk it into bytes
+
+    # a 'single char xor' still needs to be the same length as the plaintext
+    # (which is the same length as the ciphertext
+    # so, we'll create the single-char key that's the repeated byte
     h2 = Array.new(h1.length) { c }
 
-    m = h1.zip(h2).map { |l, r| l.hex ^ r }.pack("U*")
+    # we'll zip the two together, make sure that each "byte" is XOR'd with
+    # the corresponding byte and then pack it back into a string, which represents
+    # a possible plaintext
+    p1 = h1.zip(h2).map { |l, r| l.hex ^ r }.pack("C*")
 
-    [FrequencyMatch.score(m), m]
+    # return the score of the plaintext, the plaintext, and the char
+    [FrequencyMatch.score(p1), p1, c.chr]
   end
 
-puts results.sort.last[1]
+puts results.sort.last
