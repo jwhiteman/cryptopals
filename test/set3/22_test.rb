9aaa2a8a2785970e2aad5654fff468e8f5719cb3
Jim Whiteman <jimtron9000@gmail.com>
2019-07-25 16:50:40 -0700

    Set 3, challenge 22

--- test/set3/22_test.rb
+++ test/set3/22_test.rb
@@ -1,7 +1,4 @@
-# https://cryptopals.com/sets/3/challenges/21
-#
-# https://en.wikipedia.org/wiki/Mersenne_Twister
-#
+# https://cryptopals.com/sets/3/challenges/22
 require_relative "../test_helper"
 
 module Set3
@@ -74,20 +71,35 @@ module Set3
     end
   end
 
-  class Challenge21Test < Test::Unit::TestCase
-    def test_challenge_21
-      prng = MT19937.new(5489)
+  class Challenge22Test < Test::Unit::TestCase
+    def test_challenge_22
+      index = Hash.new { |h, k| h[k] = [] }
 
-      assert_equal prng.rand, 3499211612
-      assert_equal prng.rand, 581869302
-      assert_equal prng.rand, 3890346734
-      assert_equal prng.rand, 3586334585
-      assert_equal prng.rand, 545404204
-      assert_equal prng.rand, 4161255391
-      assert_equal prng.rand, 3922919429
-      assert_equal prng.rand, 949333985
-      assert_equal prng.rand, 2715962298
-      assert_equal prng.rand, 1323567403
+      start = Time.now.to_i
+
+      # we'll load up values for the next 20 minutes or so
+      (start..(start + 1200)).each do |seed|
+        prng = MT19937.new(seed)
+
+        1000.times do
+          index[prng.rand] << seed
+        end
+      end
+
+      # curmudgeonly simulating the passage of time...
+      used_seed = Time.now.to_i + rand(1000)
+
+      prng = MT19937.new(used_seed)
+
+      r1   = prng.rand
+      rand(100).times { prng.rand } # miss a few values
+      r2   = prng.rand
+      rand(100).times { prng.rand } # miss a few more values
+      r3   = prng.rand
+
+      found_seed = (index[r1] & index[r2] & index[r3]).first
+
+      assert_equal used_seed, found_seed
     end
   end
 end
