0b7cba48b9d2cd55a7f0e5031c39596b9ecdf65e
Jim Whiteman <jimtron9000@gmail.com>
2019-09-06 19:24:30 -0700

    Fix challenge 36

--- test/set5/36_test.rb
+++ test/set5/36_test.rb
@@ -3,92 +3,140 @@ require_relative "../test_helper"
 
 module Set5
   class Challenge36Test < Test::Unit::TestCase
-    # experimenting w/ a fiber here, to see if it makes it easier to write
-    # a proof-of-concept. i'm not sure that it does.
-    # regardless, treat the params of Fiber.yield as a return that then blocks
-    # until `resume` is called, which then becomes the return value.
-    def test_challenge_36
-      server = Fiber.new do |msg|
-        n, g, k, i, p = msg
+    N = "0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74"\
+      "020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374f"\
+      "e1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee38"\
+      "6bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48"\
+      "361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed52907"\
+      "7096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff".hex
+    G = 2
+    K = 3
+    I = "someone@example.com"
+    P = "some-password"
 
-        # Agree on N=[NIST Prime], g=2, k=3, I (email), P (password)
-        # ...and get i and biga in return
-        i, biga = Fiber.yield(msg)
-
-        # Generate salt as random integer
-        salt = rand(n)
-
-        # Generate string xH=SHA256(salt|password)
-        xh   = OpenSSL::Digest::SHA256.hexdigest(salt.to_s + p)
-
-        # Convert xH to integer x somehow (put 0x on hexdigest)
-        x    = xh.hex
-
-        # Generate v=g**x % N
-        v    = g.pow(x, n)
-
-        # Send salt, B=kv + g**b % N
-        b    = rand(n)
-        bigb = k * v + g.pow(b, n)
-
-        client_mac = Fiber.yield([salt, bigb])
-
-        # Compute string uH = SHA256(A|B), u = integer of uH
-        uh = OpenSSL::Digest::SHA256.hexdigest(biga.to_s + bigb.to_s)
-        u  = uh.hex
-
-        # Generate S = (A * v**u) ** b % N
-        s  = (biga * v.pow(u, n)).pow(b, n)
-
-        # Generate K = SHA256(S)
-        k  = OpenSSL::Digest::SHA256.hexdigest(s.to_s)
-
-        # Send "OK" if HMAC-SHA256(K, salt) validates
-        our_mac = OpenSSL::HMAC.hexdigest("SHA1", k, salt.to_s)
-
-        Fiber.yield (client_mac == our_mac) ? "OK" : [client_mac, our_mac]
+    module CommonHelpers
+      def _hash(*args)
+        OpenSSL::Digest::SHA256.hexdigest(
+          args.map(&:to_s).reduce(:+)
+        )
       end
 
-      n = "0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74"\
-        "020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374f"\
-        "e1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee38"\
-        "6bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48"\
-        "361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed52907"\
-        "7096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff".hex
+      def _hmac(k, salt)
+        OpenSSL::HMAC.hexdigest("SHA256", k.to_s, salt.to_s)
+      end
+    end
 
-      g = 2
-      k = 3
-      i = "someone@example.com"
-      p = "some-password"
+    class Server
+      attr_reader :db
 
-      n, g, k, i, p = server.resume([n, g, k, i, p])
+      def initialize
+        @db = {}
+      end
 
-      a    = rand(n)
-      biga = g.pow(a, n)
+      def register(i, s, v)
+        db[i] = { s: s, v: v }
+      end
 
-      # Send I, A=g**a % N (a la Diffie Hellman)
-      salt, bigb = server.resume([i, biga])
+      def lookup(i)
+        db[i]
+      end
+    end
 
-      # Compute string uH = SHA256(A|B), u = integer of uH
-      uh = OpenSSL::Digest::SHA256.hexdigest(biga.to_s + bigb.to_s)
-      u  = uh.hex
+    class Worker
+      include CommonHelpers
 
-      # Generate string xH=SHA256(salt|password)
-      xh = OpenSSL::Digest::SHA256.hexdigest(salt.to_s + p)
+      attr_reader :server
+      attr_reader :n
+      attr_reader :g
+      attr_reader :b
+      attr_reader :public_b
+      attr_reader :public_a
+      attr_reader :i
+      attr_reader :s
+      attr_reader :v
 
-      # Convert xH to integer x somehow (put 0x on hexdigest)
-      x  = xh.hex
+      def initialize(server, n, g)
+        @server = server
+        @n      = n
+        @g      = g
+        @b      = rand(n)
+      end
 
-      # Generate S = (B - k * g**x)**(a + u * x) % N
-      s = (bigb - k * g.pow(x, n)).pow(a + u * x, n)
+      def exchange_keys(i, public_a)
+        @public_a  = public_a
+        @i         = server.lookup(i)
+        @s         = @i[:s]
+        @v         = @i[:v]
+        @public_b  = (K * v) + g.pow(b, n)
 
-      # Generate K = SHA256(S)
-      k = OpenSSL::Digest::SHA256.hexdigest(s.to_s)
+        [@s, @public_b]
+      end
 
-      # Send HMAC-SHA256(K, salt)
-      mac = OpenSSL::HMAC.hexdigest("SHA1", k, salt.to_s)
+      def verify_mac(client_mac)
+        u       = _hash(public_a, public_b).hex
+        s       = (public_a * v.pow(u, n)).pow(b, n)
+        k       = _hash(s)
+        our_mac = _hmac(k, @s)
 
-      response = server.resume(mac)
+        if client_mac == our_mac
+          "OK"
+        else
+          "ERROR"
+        end
+      end
+    end
+
+    class Client
+      include CommonHelpers
+
+      attr_reader :i
+      attr_reader :p
+      attr_reader :n
+      attr_reader :g
+      attr_reader :x
+      attr_reader :s
+      attr_reader :v
+
+      def initialize(i, p, n, g)
+        @i      = i                  # identifier (email)
+        @p      = p                  # password
+        @n      = n                  # large prime number
+        @g      = g                  # generator
+      end
+
+      def register(server)
+        @s      = rand(n)            # salt
+        @x      = _hash(@s, @p).hex  # ...some intermediate value
+        @v      = g.pow(x, n)        # verifier
+
+        server.register(@i, @s, @v)
+      end
+
+      def login(worker)
+        a              = rand(n)
+        public_a       = g.pow(a, n)
+
+        # step 1
+        salt, public_b = worker.exchange_keys(@i, public_a)
+
+        u              = _hash(public_a, public_b).hex
+        s              = (public_b - K * g.pow(x, n)).pow(a + u * x, n)
+        k              = _hash(s)
+        mac            = _hmac(k, salt)
+
+        # step 2
+        worker.verify_mac(mac)
+      end
+    end
+
+    def test_challenge_36
+      server   = Server.new
+      client   = Client.new(I, P, N, G)
+      client.register(server)
+
+      worker   = Worker.new(server, N, G)
+
+      response = client.login(worker)
 
       assert_equal response, "OK"
     end
---
+++ test/set5/37_test.rb
@@ -0,0 +1,9 @@
+# https://cryptopals.com/sets/5/challenges/37
+require_relative "../test_helper"
+
+module Set5
+  class Challenge37Test < Test::Unit::TestCase
+    def test_challenge_37
+    end
+  end
+end
